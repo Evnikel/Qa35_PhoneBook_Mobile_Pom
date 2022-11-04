bd865c1f7e88416f64bcc33df503cc4b3222e45b
Evnikel <evnikel@gmail.com>
2022-11-04 12:27:57 +0200

    added classes

---
+++ src/main/java/screens/AddNewContactScreen.java
@@ -0,0 +1,11 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+
+public class AddNewContactScreen extends BaseScreen{
+    public AddNewContactScreen(AppiumDriver<AndroidElement> driver) {
+        super(driver);
+    }
+
+}
---
+++ src/main/java/screens/LoginRegistrationScreen.java
@@ -0,0 +1,16 @@
+package screens;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginRegistrationScreen extends BaseScreen{
+    
+    public LoginRegistrationScreen(AppiumDriver<AndroidElement> driver) {
+        super(driver);
+    }
+
+    @FindBy (xpath = "")
+    AndroidElement editTextEmail;
+
+}
